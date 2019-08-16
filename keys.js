8555ee9ff431e6e946d3b4e774ed2d3377f81d13
rebeccahdz10 <rebeccahdz10@gmail.com>
2019-08-16 15:50:42 -0700

    added more var for new ctrs

--- keys.js
+++ keys.js
@@ -4,3 +4,11 @@ exports.spotify = {
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
 };
+
+exports.omdb = {
+    id: process.env.OMDB_ID,
+};
+
+exports.bandsInTown = {
+    id: process.env.BIT_ID,
+};
--- liri.js
+++ liri.js
@@ -3,3 +3,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
+
+var omdb = new omdb(keys.omdb);
+
+var bandsInTown = new bandsInTown(keys.bandsInTown);
