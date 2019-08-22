2520ecb3c95696bdaa496afef35081e2293dd60e
rebeccahdz10 <rebeccahdz10@gmail.com>
2019-08-22 09:19:11 -0700

    changed a bunch of stuff, and still no error message

--- search.js
+++ search.js
@@ -7,17 +7,23 @@ var search = function () {
 
     this.findSong = function (term) {
 
+        console.log("I'm trying!");
+
         var spotify = new Spotify({
             id: "18d4241390954cc99ba5e4d2e44178f1",
             secret: "7316f255d7bb470fb5b788a859143aa8"
         });
 
-        var term = process.argv.slice(3).join("");
+        // var term = process.argv.slice(3).join("");
 
         spotify.search({ type: 'track', query: term }, function (err, response) {
+
             if (err) {
                 return console.log('Error occurred: ' + err);
-            } else {
+            }
+            else {
+                var song = response.tracks.items
+
                 for (var i = 0; i < response.length; i++) {
                     var jsonData = JSON.parse(response.data[i].tracks.items);
 
